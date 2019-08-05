07a5993dc9e22281f14f63e3774d485b60e53653
zalo <makeshifted@gmail.com>
2019-08-05 12:23:24 -0700

    Update the note app to actually create the gitographer repo

--- js/noteApp.js
+++ js/noteApp.js
@@ -1,20 +1,73 @@
 var CreateGitographer = function (githubAccessToken) {
+    this.githubAccessToken = githubAccessToken;
+
     this.initialize = function(){
-        console.log("Initializing Gitographer, access token:" + githubAccessToken);
-        this.gh = new GitHub({token: githubAccessToken});
+        console.log("Initializing Gitographer, access token:" + this.githubAccessToken);
 
-        console.log(this.gh);
-        let githubUser = this.gh.getUser("zalo");
-        console.log(githubUser);
+        this.ghGet("user", (userData)=>{
+            this.githubUser = userData;
+            console.log(this.githubUser);
 
-        githubUser.listRepos()
-            .then(function({data: reposJson}) {
-                console.log(githubUser.nickname+` has ${reposJson.length} repos!`);
-            });
+            // Check to see if this user already has a Gitographer-Notes repo
+            this.ghGet("repos/"+this.githubUser.login+"/gitographer-notes", 
+                (repoData) => {
+                    // Hooray!  Begin setting up the app with data downloaded from the repo...
+                    this.repo = repoData; 
+                    console.log(repoData); 
+                },
+                (error) => {
+                    this.repo = null;
+                    if(error.message === "Not Found") {
+                        console.log("You don't have a Gitographer repository yet!!!");
+                    } else {
+                        console.log("Something broke in an unexpected manner...");
+                        console.log(error);
+                    }
+                });
+        });
     }
 
     this.createRepository = function(){
-        console.log("Create a Gitograpgher Repository and add files to it.");
+        console.log("Create a Gitographer Repository and add files to it.");
+        let repositorySettings = {
+          "name": "gitographer-notes",
+          "description": "Your private Gitographer storage space",
+          "homepage": "https://gitographer.com",
+          "private": true,
+          "has_issues": false,
+          "has_projects": false,
+          "has_wiki": false
+        };
+
+        this.ghPost('user/repos', repositorySettings, (repoInfo)=>{
+            console.log(repoInfo);
+        });
+    }
+
+    this.ghGet = function(endpoint, callback, errorCallback){
+        fetch('https://api.github.com/'+endpoint, {
+            method: 'GET', headers: { 'Authorization': 'token '+this.githubAccessToken }
+        }).then((response) => {
+            response.json().then((parsedData)=>{
+                callback(parsedData);
+            });
+        }).catch((error) => errorCallback(error));
+    }
+
+    this.ghPost = function(endpoint, data, callback, errorCallback){
+        return fetch('https://api.github.com/'+endpoint, {
+            method: 'POST',
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                'Authorization': 'token '+this.githubAccessToken,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        }).then((response) => {
+            response.json().then((parsedData)=>{
+                callback(parsedData);
+            });
+        }).catch((error) => errorCallback(error));
     }
 
     this.initialize();
